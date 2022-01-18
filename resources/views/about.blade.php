0fb6c3c548a1f5728ffbdd49e1a206a64ff75ce3
NisrinaAmaliaIf <nisrinaamaliaiffatunnisa@gmail.com>
2022-01-18 21:17:00 +0700

    praktik 01-laravel nisrina

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel= "stylesheet" href= "css/style.css">
+    <title>Nisrina | About</title>
+</head>
+<body>
+    <h1><center>Halaman About</center></h1>
+    <h3><?php echo $nama; ?></h3>
+    <p><?php echo $email; ?><p>
+    <img src= "images/nisrina.JPG" alt="nisrina" width="200px">
+</body>
+</html>
